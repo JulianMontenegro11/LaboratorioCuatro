c449a054126499b7a19d3bb1fc06fd6db2d68556
Julian <jmontengro.pinzon111mail.com>
2021-08-26 23:09:08 -0500

    neutro

--- main.cpp
+++ main.cpp
@@ -3,20 +3,6 @@
 int main()
 {
    red inv;
-   inv.agregar_nodo('A');
-   inv.agregar_nodo('B');
-   inv.agregar_nodo('C');
-   inv.agregar_nodo('D');
-   inv.Visualizar_matriz();
-   inv.modificar_nodo('A');
-   inv.Visualizar_matriz();
-   inv.modificar_nodo('C');
-   inv.Visualizar_matriz();
-   inv.eliminar_nodo('B');
-   inv.Visualizar_matriz();
-   inv.agregar_nodo('B');
-   inv.Visualizar_matriz();
-   inv.agregar_nodo('F');
-   inv.Visualizar_matriz();
+
    return 0;
 }
